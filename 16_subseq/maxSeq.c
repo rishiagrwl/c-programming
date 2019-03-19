3bd83512b9004dde5d29659648fe2b4691fbb052
Coursera Learner <nobody@nowhere.nul>
2019-03-19 09:22:12 +0000

    Done assig nment 16_subseq

--- 16_subseq/maxSeq.c
+++ 16_subseq/maxSeq.c
@@ -3,8 +3,9 @@
 
 size_t maxSeq(int * array, size_t n){
   int i, l=1, m=1;
-  if(n==0 || array == NULL)
-    m=0;
+  if(n==0 || array == NULL){
+    l=0;
+    m=0;}
   else
     for(i=1; i<n; i++){
       if(array[i]>array[i-1])
